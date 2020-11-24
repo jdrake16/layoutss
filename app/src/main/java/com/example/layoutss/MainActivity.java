3434cc177cf72bfcaa3e5bd2bb058d9d2e71c2b0
jdrake16 <stovetops111@gmail.com>
2020-11-24 00:36:45 -0700

    basic fade out/in animation

--- app/src/main/java/com/example/layoutss/MainActivity.java
+++ app/src/main/java/com/example/layoutss/MainActivity.java
@@ -3,8 +3,24 @@ package com.example.layoutss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+
+        ImageView zim = findViewById(R.id.imageView_zim);
+
+        ImageView smooth = findViewById(R.id.imageView_smooth);
+
+        zim.animate().alpha(0f).setDuration(2000);
+
+        smooth.animate().alpha(1).setDuration(2000);
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
